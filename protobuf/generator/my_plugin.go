c17814bc966fd85d977815b9b6c9ec8a039ded89
wangchao.zhuanxu <wanghchao.zhuanxu@bytedance.com>
2018-08-21 13:20:29 +0800

    实现简单插件，完成输出
    Change-Id: I394b8b42edbb11745f1abde4c19272aa9a587f78

--- protobuf/generator/my_plugin.go
+++ protobuf/generator/my_plugin.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
 	"github.com/golang/protobuf/proto"
-	"fmt"
+	"encoding/json"
 )
 
 func Error(err error, msgs ...string) {
@@ -21,13 +21,20 @@ func Fail(msgs ...string) {
 	os.Exit(1)
 }
 
+type JsonFormat struct {
+	Typ      string           `json:"type"`
+	Filename string           `json:"filename"`
+	Name     string           `json:"name"`
+	Values   map[string]int32 `json:"values"`
+}
+
 func main() {
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		Error(err, "reading input")
 	}
 	request := new(plugin.CodeGeneratorRequest)
-	if err  := proto.Unmarshal(data, request); err != nil{
+	if err := proto.Unmarshal(data, request); err != nil {
 		Error(err, "parsing input proto")
 	}
 	if len(request.FileToGenerate) == 0 {
@@ -39,7 +46,39 @@ func main() {
 	// 参数是什么？
 	//ouput := make(map[string]string)
 	f := request.ProtoFile[0]
-	for _, e := range f.EnumType {
-		fmt.Println(e)
+	e := f.GetEnumType()[0]
+
+	output := new(JsonFormat)
+	output.Name = e.GetName()
+	output.Filename = f.GetName()
+	output.Typ = "Enum"
+	values := make(map[string]int32)
+	for _, v := range e.GetValue() {
+		values[v.GetName()] = v.GetNumber()
+	}
+	output.Values = values
+
+	response := new(plugin.CodeGeneratorResponse)
+
+	content, err := json.Marshal(output)
+	if err != nil {
+		Error(err, "failed to marshal output json")
+	}
+	//ioutil.WriteFile("test.txt", content, os.ModePerm)
+	//fmt.Println(content)
+	//os.Exit(1)
+	response.File = append(response.File, &plugin.CodeGeneratorResponse_File{
+		Name:    proto.String("hello.json"),
+		Content: proto.String(string(content)),
+	})
+
+	// Send back the results.
+	data, err = proto.Marshal(response)
+	if err != nil {
+		Error(err, "failed to marshal output proto")
+	}
+	_, err = os.Stdout.Write(data)
+	if err != nil {
+		Error(err, "failed to write output proto")
 	}
 }
